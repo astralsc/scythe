0e7b51db6332272f5b046d1a9aec7aab3d1d8c0f
Astral <132731805+astralsc@users.noreply.github.com>
2026-06-28 17:23:53 +0800

    add update option and updated php ini

--- start.bat
+++ start.bat
@@ -4,19 +4,23 @@ title Scythe
 :menu
 cls
 echo 1. Setup
-echo 2. Start
-echo 3. Setup and Start
-echo 4. Cleanup
-echo 5. Exit
+echo 2. Update
+echo 3. Start
+echo 4. Setup and Start
+echo 5. Setup, Update and Start
+echo 6. Cleanup
+echo 7. Exit
 echo.
 
 set /p choice=Choose an option: 
 
 if "%choice%"=="1" goto setup
-if "%choice%"=="2" goto start
-if "%choice%"=="3" goto both
-if "%choice%"=="4" goto cleanup
-if "%choice%"=="5" exit
+if "%choice%"=="2" goto update
+if "%choice%"=="3" goto start
+if "%choice%"=="4" goto both
+if "%choice%"=="5" goto triple
+if "%choice%"=="6" goto cleanup
+if "%choice%"=="7" exit
 
 cls
 echo Invalid choice.
@@ -27,6 +31,13 @@ goto menu
 cd laravel
 cls
 call setup.bat
+pause
+exit
+
+:update
+cd laravel
+cls
+call update.bat
 pause
 exit
 
@@ -42,6 +53,15 @@ cd laravel
 cls
 call setup.bat
 call start.bat
+pause
+exit
+
+:triple
+cd laravel
+cls
+call setup.bat
+call start.bat
+call update.bat
 pause
 exit
 
